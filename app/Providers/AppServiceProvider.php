4996429e6d5bb013b971bc1ce8bb10f1082e76bb
Asep Ramdan <80875965+asepramdhan@users.noreply.github.com>
2025-07-24 22:09:30 +0700

    update
    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,9 @@ use App\Http\Middleware\IsAdmin;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +26,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Schema::defaultStringLength(191);
+        // arahkan ke lokasi build di hosting
+        // Vite::useBuildDirectory('../../public_html/build');
+
+        Vite::useBuildDirectory('build');
+
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
+
         // register middleware
         Route::aliasMiddleware('is_admin', IsAdmin::class);
         Blade::if('admin', function () {
